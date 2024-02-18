459aa52c114be963dd6b35d5a1d089c87b3187e3
ricardo silva <rs.silva.1985@hotmail.com>
2024-02-18 21:19:24 +0000

    feature:add,entity,service,and repository

---
+++ virtualMarket-api/user-service/src/main/java/pt/virtualmarket/userservice/entities/UserEntity.java
@@ -0,0 +1,27 @@
+package pt.virtualmarket.userservice.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name="users")
+public class UserEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+    @Column(name = "name")
+    private String name;
+    @Column(name = "email")
+    private String email;
+    @Column(name = "age")
+    private String age;
+
+
+}
